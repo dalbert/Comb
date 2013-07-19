8fe4f5d785f12e31c8cac0b01ce4ebc4be429f8e
David Albert <david@bronto.com>
2013-07-19 11:29:51 -0600

    draws one hexagon using paperjs vector and path.lineBy()

--- comb.js
+++ comb.js
@@ -6,9 +6,20 @@ var fart = function() {
 	var start = new Point(100, 100);
 	// Move to start and draw a line from there
 	path.moveTo(start);
+
+	var point = new Point({
+		length: 100,
+		angle: 0
+	});
 	// Note the plus operator on Point objects.
 	// PaperScript does that for us, and much more!
-	path.lineTo(start + [ 100, -50 ]);
+
+	path.lineBy(point);
+	path.lineBy(point.rotate(60));
+	path.lineBy(point.rotate(120));
+	path.lineBy(point.rotate(180));
+	path.lineBy(point.rotate(240));
+	path.lineBy(point.rotate(300));
 };
 
 fart();
